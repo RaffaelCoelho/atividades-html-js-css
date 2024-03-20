781d7574527ce4789c509492519bd1bd0d22a8bf
TocaDoCoelho <144127648+RaffaelCoelho@users.noreply.github.com>
2024-03-20 10:50:27 -0300

    Update relogio.js

--- relogio.js
+++ relogio.js
@@ -19,6 +19,16 @@ function relogio(){
     let m = dia.getMinutes()
     let s = dia.getSeconds()
 
+    if(h<10){
+    h="0"+h
+ }
+
+ if(m<10){
+    m="0"+h
+ }
+ if(s<10){
+    s="0"+h
+ }
     horas.textContent = h
     minutos.textContent = m
     segundos.textContent = s
